f91d65a4868e1876aaad7d9f76c104cfcc0de92f
Anderson de Oliveira Lima FIlho <anderson.filho@parvi.local>
2024-07-05 11:44:12 -0300

    Desafio do nomes maiusculos e minusculos

---
+++ desafio_22.py
@@ -0,0 +1,19 @@
+#Pegue o nome
+#print nome completo
+#print nome minusculo
+#Quantas letras tem no nome
+#e a quantidade de letras no primeiro nome
+
+nome = input("Nome completo \n")
+print('Analisando seu nome.... ')
+print(nome.upper())
+print(nome.lower())
+'''print(f'Seu nome tem {len(nome)-nome.count(' ')} letras')
+novo = ''
+for i in nome:
+    if i == ' ':
+        break
+    else:
+        novo += i
+print(f'Primeiro nome é {novo}')'''
+print(nome.split[0])
